8e91765fc3e861b96e0ae1bf9a7383633481ce23
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-03-16 16:04:23 +0900

    Homework Doing

--- Week2/HomeWork/Week2-Practice.py
+++ Week2/HomeWork/Week2-Practice.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
 
 LoadInput = np.load("./Week2-data_input.npy")
 LoadTarget = np.load("./Week2-data_target.npy")
@@ -20,11 +21,18 @@ mean = np.mean(train_input, axis=0)
 std = np.std(train_input, axis=0)
 train_scaled = (train_input - mean)
 test_scaled = (test_input - mean)
+new = ([25, 500] - mean)
 
 kn.fit(train_scaled, train_target)
 print(kn.score(test_scaled, test_target))
+print(kn.predict([new]))
 
-plt.scatter(train_scaled[:,0], train_scaled[:,1])
+distances, indexes = kn.kneighbors([new])
+
+plt.scatter(train_scaled[:, 0], train_scaled[:, 1], s=20, c="black")
+plt.scatter(new[0], new[1], marker="^")
+plt.scatter(train_scaled[indexes, 0], train_scaled[indexes, 1], marker="D")
+
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.show()
