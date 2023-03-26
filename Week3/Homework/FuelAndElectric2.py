92dc6d01cab43f103701b2729e3118865946d9d8
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-03-26 18:32:44 +0900

    Homework Comment

--- Week3/Homework/FuelAndElectric2.py
+++ Week3/Homework/FuelAndElectric2.py
@@ -6,12 +6,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
-# 데이터 불러오기
+# 데이터 불러오기(Pandas)
 input_data = pd.read_excel('Fuel_Power.xlsx', header=None)
 input_target = pd.read_excel('Electric_Power.xlsx', header=None)
 
-# 첫번째 열 제거
+# 입력 데이터와 타켓 데이터의 차이
+# 회귀 분석 : 연속적인 출력값을 예측하는 것
+# 회귀는 예측 변수(Predictor Variable, 설명변수(Explanatory Variable), 입력(Input))와
+# 연속적인 반응 변수(Response Variable, 출력(Output), 타깃(Target))가
+# 주어졌을 때 두 변수 사이의 관계를 찾음
+# 즉, 우리가 흔히 얘기하는 y = ax + b의 형태를 갖는 것
+
+# 현재 데이터 분포도 확인
+input_data.plot()
+input_target.plot()
+plt.show()
+
+# 첫번째 열 제거(필요없는 데이터)
 input_data = input_data.drop(columns=[0])
+
+# 데이터 확인
+print(input_data)
+print(input_target)
 
 # Array로 형변환
 input_data = np.array(input_data)
@@ -27,8 +43,20 @@ print(len(test_input))
 print(len(test_target))
 
 # 2차원 배열로 표시
+# 이유 : sklearn에서는 벡터와 행렬을 나타내는 방법으로 numpy 배열을 표준으로 함.
+# 따라서 x와 y를 각각 numpy.array로 변환해야함.
+# 하나의 속성(feature)에 여러가지 값(sample)을 갖는 경우, reshape(-1, 1)을 적용하여 열벡터로 만들어야 함.
+# X는 하나의 종속 변수 Y에 대한 여러가지 값을 가지기 때문
+# 여기서 reshape(-1, 1)의 -1의 의미는
+# 남은 열의 값은 특정 정수로 지정이 되었을 때,
+# 남은 배열의 길이와 남은 차원으로부터 추정해서 알아서 하라는 뜻
 train_input = train_input.reshape(-1, 2)
 test_input = test_input.reshape(-1, 2)
+
+# 1번째 열과 2번째 열의 차이를 보여줌
+plt.scatter(train_input[:, 0], train_target[:, 0])
+plt.scatter(train_input[:, 1], train_target[:, 0])
+plt.show()
 
 # 표준화
 mean = np.mean(train_input, axis=0)
@@ -41,26 +69,28 @@ lr = LinearRegression()
 lr.fit(train_input, train_target)
 
 # 기울기와 절편 구하기
+# lr.coef : 기울기
+# lr.intercept : 절편
+# _ : ML에서 유도된 결과를 나타내는 기호
 print(lr.coef_, lr.intercept_)
 print("coef =", lr.coef_)
 print("intercept =", lr.intercept_)
 
+# 현재 1차원 선형 데이터로 학습시켰을 때 학습 정확도와 테스트 정확도
+print("--------------- 선형 회귀(1차) ---------------")
+print("----------------- 학습 정확도 ----------------")
 print(lr.score(train_input, train_target))
+print("---------------- 테스트 정확도 ---------------")
 print(lr.score(test_input, test_target))
-
-# print(lr.predict([[2000000, 1500000]]))		# 215.0
-
-# 1번째 열과 2번째 열의 차이를 보여줌
-plt.scatter(train_input[:, 0], train_target[:, 0])
-plt.scatter(train_input[:, 1], train_target[:, 0])
-plt.show()
+print("----------------------------------------------")
 
 # PolynomialFeatures = 다항회귀
 # 데이터의 형태가 비선형일때, 데이터에 각 특성의 제곱을 추가하여
 # 특성이 추가된 비선형 데이터를 선형 회귀 모델로 훈련시키는 방법
 # 쉽게 생각하면, degree값(차수)의 변화에 따라 정확도가 달라지게 됨
+# 기본 degree의 default = 2
 # include_bias = 0차항의 유무
-poly = PolynomialFeatures(degree=3, include_bias=False)
+poly = PolynomialFeatures(degree=2, include_bias=False)
 
 # 학습 모델을 학습
 poly.fit(train_input)
@@ -74,17 +104,25 @@ test_poly = poly.transform(test_input)
 # train_poly.shape = (n_samples, n_features)와 같은 형태를 가지며
 # train_poly에 포함된 행과 열의 개수 의미
 # 118, 6이면 118개의 샘플데이터와 각 샘플은 6개의 특징을 가짐
+print("--------------- train_poly.shape ---------------")
 print(train_poly.shape)				# 118, 6
+print("------------------------------------------------")
 
 # 모든 다항식 특징의 이름을 반환
+print("--------------- poly.get_feature_names_out() ---------------")
 print(poly.get_feature_names_out())
+print("------------------------------------------------------------")
 
 # train_poly와 train_target을 학습
 lr.fit(train_poly, train_target)
 
 # 정확도 예측
+print("--------------- 선형 회귀(2차, PolynomialFeatures) ---------------")
+print("--------------------------- 학습 정확도 --------------------------")
 print(lr.score(train_poly, train_target))
+print("-------------------------- 테스트 정확도 -------------------------")
 print(lr.score(test_poly, test_target))
+print("------------------------------------------------------------------")
 
 # Regularization
 # StandardScaler = 주어진 데이터셋의 각 특징을 평균이 0이고
@@ -99,9 +137,12 @@ test_scaled = ss.transform(test_poly)
 # Ridge Model
 ridge = Ridge()
 ridge.fit(train_scaled, train_target)
-
+print("--------------- Ridge Model, 규제 강도 없음 ---------------")
+print("----------------------- 학습 정확도 -----------------------")
 print(ridge.score(train_scaled, train_target))
+print("---------------------- 테스트 정확도 ----------------------")
 print(ridge.score(test_scaled, test_target))
+print("------------------------------------------------------------")
 
 alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
 
@@ -125,15 +166,23 @@ plt.show()
 ridge = Ridge(alpha=1)
 ridge.fit(train_scaled, train_target)
 
+print("--------------- Ridge Model, 규제 강도 있음, alpha = 1 ---------------")
+print("----------------------------- 학습 정확도 ----------------------------")
 print(ridge.score(train_scaled, train_target))
+print("---------------------------- 테스트 정확도 ---------------------------")
 print(ridge.score(test_scaled, test_target))
+print("-----------------------------------------------------------------------")
 
 # Lasso Model
 lasso = Lasso()
 lasso.fit(train_scaled, train_target)
 
+print("--------------- Lasso Model, 규제 강도 없음 ---------------")
+print("----------------------- 학습 정확도 -----------------------")
 print(lasso.score(train_scaled, train_target))
+print("---------------------- 테스트 정확도 ----------------------")
 print(lasso.score(test_scaled, test_target))
+print("-----------------------------------------------------------")
 
 for alpha in alpha_list:
 	lasso = Lasso(alpha = alpha)
@@ -150,5 +199,9 @@ plt.show()
 lasso = Lasso(alpha=0.1)
 lasso.fit(train_scaled, train_target)
 
-print(ridge.score(train_scaled, train_target))
-print(ridge.score(test_scaled, test_target))
+print("--------------- Lasso Model, 규제 강도 있음, alpha = 0.1 ---------------")
+print("------------------------------ 학습 정확도 -----------------------------")
+print(lasso.score(train_scaled, train_target))
+print("----------------------------- 테스트 정확도 ----------------------------")
+print(lasso.score(test_scaled, test_target))
+print("------------------------------------------------------------------------")
