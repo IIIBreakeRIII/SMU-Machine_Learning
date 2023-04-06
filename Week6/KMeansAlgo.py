30fdaec3a6120d4ca4cd688a77906ace8957a125
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-04-06 17:39:39 +0900

    KMeans Algo

--- Week6/KMeansAlgo.py
+++ Week6/KMeansAlgo.py
@@ -2,12 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 
 fruits = np.load('fruits_300.npy')
-
 fruits_2d = fruits.reshape(-1, 100*100)
 
 km = KMeans(n_clusters=3, random_state=42)
 km.fit(fruits_2d)
 
+def draw_fruits(arr, ratio=1):
+	n = len(arr)
+	rows = int(np.ceil(n/10))
+	cols = n if rows < 2 else 10
+	fig, axs = plt.subplots(rows, cols, figsize=(cols*ratio, rows*ratio), squeeze=False)
+
+	for i in range(rows):
+		for j in range(cols):
+			if i * 10 + j < n:
+				axs[i, j].imshow(arr[i * 10 + j], cmap='gray_r')
+			axs[i, j].axis('off')
+
+draw_fruits(fruits[km.labels_==0])
+
 print(km.labels_)
+
+pca = PCA(n_components=50)
+pca.fit(fruits_2d)
+
+print(pca.components_.shape)
+
+draw_fruits(pca.components_.reshape(-1, 100, 100))
+
+print(fruits_2d.shape)
+
+fruits_pca = pca.transform(fruits_2d)
+print(fruits_pca.shape)
+
+fruits_inverse = pca.inverse_transform(fruits_pca)
+print(fruits_inverse.shape)
+fruits_reconstruct = fruits_inverse.reshape(-1, 100, 100)
+
+print(np.sum(pca.explained_variance_ratio_))
+
+plt.plot(pca.explained_variance_ratio_)
+plt.show()
