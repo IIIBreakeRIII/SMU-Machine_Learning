176268bf3debde432b7e882636a205407d4c4910
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-04-06 16:47:10 +0900

    Week 6 Lecture

--- Week6/UnSupervisedLearning.py
+++ Week6/UnSupervisedLearning.py
@@ -21,3 +21,33 @@ plt.hist(np.mean(pineapple, axis=1), alpha=0.8)
 plt.hist(np.mean(banana, axis=1), alpha=0.8)
 plt.legend(['apple', 'pineapple', 'banana'])
 plt.show()
+
+fig, axs = plt.subplots(1, 3, figsize=(20, 5))
+axs[0].bar(range(10000), np.mean(apple, axis=0))
+axs[1].bar(range(10000), np.mean(pineapple, axis=0))
+axs[2].bar(range(10000), np.mean(banana, axis=0))
+plt.show()
+
+# 평균 이미지 그리기
+apple_mean = np.mean(apple, axis=0).reshape(100, 100)
+pineapple_mean = np.mean(pineapple, axis=0).reshape(100, 100)
+banana_mean = np.mean(banana, axis=0).reshape(100, 100)
+
+fig, axs = plt.subplots(1, 3, figsize=(20, 5))
+axs[0].imshow(apple_mean, cmap='gray_r')
+axs[1].imshow(pineapple_mean, cmap='gray_r')
+axs[2].imshow(banana_mean, cmap='gray_r')
+plt.show()
+
+# 평균과 가까운 사진 고르기
+abs_diff = np.abs(fruits - apple_mean)
+abs_mean = np.mean(abs_diff, axis=(1, 2))
+print(abs_mean.shape)
+
+apple_index = np.argsort(abs_mean[:100])
+fig, axs = plt.subplots(10, 10, figsize=(10, 10))
+for i in range(10):
+	for j in range(10):
+		axs[i, j].imshow(fruits[apple_index[i*10 + j]], cmap='gray_r')
+		axs[i, j].axis('off')
+plt.show()
