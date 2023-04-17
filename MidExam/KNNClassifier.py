d28fab75336997262d09878a9cfa3bdbf8da3b94
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-04-17 22:55:02 +0900

    Logistic

---
+++ MidExam/KNNClassifier.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from scipy.special import expit
+
+fish = pd.read_csv('https://bit.ly/fish_csv_data')
+fish.head()
+
+fish_input = fish[['Weight', 'Length', 'Diagonal', 'Height', 'Width']].to_numpy()
+fish_target = fish['Species'].to_numpy()
+
+train_input, test_input, train_target, test_target = train_test_split(fish_input, fish_target, random_state=42)
+
+ss = StandardScaler()
+ss.fit(train_input)
+
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+
+kn = KNeighborsClassifier(n_neighbors=3)
+kn.fit(train_scaled, train_target)
+
+# Class 이름
+print(kn.classes_)
+
+print(kn.predict(test_scaled[:5]))
+# test_scaled의 예측 확률을 계산
+proba = kn.predict_proba(test_scaled[:5])
+
+print(np.round(proba, decimals=4))
+
+# Logistic Regression
+
+bream_smelt_indexes = (train_target == 'Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+print(lr.predict(train_bream_smelt[:5]))
+print(lr.predict_proba(train_bream_smelt[:5]))
+
+# 로지스틱 회귀 계수 확인	
+print(lr.coef_, lr.intercept_)
+
+# 로지스틱 회귀 계수에 각 무게, 길이, 대각선, 높이, 두께를 넣은 값
+decisions = lr.decision_function(train_bream_smelt[:5])
+print(decisions)
+
+print(expit(decisions))
