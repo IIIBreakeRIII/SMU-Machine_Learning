5d2e2ef28aa96c8da7b01ae1898a6d4a77a8f72e
IIIBreakeRIII <89850286+IIIBreakeRIII@users.noreply.github.com>
2023-05-07 17:20:55 +0900

    Sum and Sub done

--- Week9/SubtractionCalc.py
+++ Week9/SubtractionCalc.py
@@ -15,7 +15,7 @@ for _ in range(1000):
 	num2.append(random.randint(1, 1000) / 100)
 
 for i in range(1000):
-	ans.append(num1[i] + num2[i])
+	ans.append(num1[i] - num2[i])
 
 input_list = np.column_stack([num1, num2])
 target_list = np.array(ans).reshape(1000, 1)
@@ -23,22 +23,22 @@ target_list = np.array(ans).reshape(1000, 1)
 train_scaled, val_scaled, train_target, val_target = train_test_split(input_list, target_list, test_size=0.2, random_state=42)
 
 model = keras.Sequential()
-model.add(keras.layers.Dense(10, input_dim=2, activation='relu'))
-model.add(keras.layers.Dense(20, activation='relu'))
-model.add(keras.layers.Dense(5, activation='relu'))
-model.add(keras.layers.Dense(1, activation='relu'))
+model.add(keras.layers.Dense(10, input_dim=2, activation='linear'))
+model.add(keras.layers.Dense(20, activation='linear'))
+model.add(keras.layers.Dense(5, activation='linear'))
+model.add(keras.layers.Dense(1, activation='linear'))
 
 model.compile(optimizer='adam', loss='mse', metrics='accuracy')
 model.fit(train_scaled, train_target, batch_size=1, epochs=5)
 
 model.evaluate(val_scaled, val_target)
 
-result1 = model.predict([[100, 200]])
-print(">> 100 + 200")
-print(">> 300")
+result1 = model.predict([[1, 2]])
+print(">> 1 - 2")
+print(">> -1")
 print(result1[0])
 
-result2 = model.predict([[203.91, 23.313]])
-print(">> 203.91 + 23.313")
-print(">>", 203.91 + 23.313)
+result2 = model.predict([[100.11, 201.11]])
+print(">> 100.11 - 201.11")
+print(">>", 100.11 - 201.11)
 print(result2[0])
